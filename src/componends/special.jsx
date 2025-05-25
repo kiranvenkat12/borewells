9d8d638f95f6b4302c0ae4f6fd035e248f1d98ba
kiranvenkat12 <beesettyk@gmail.com>
2025-05-25 16:37:52 +0530

    Update special.jsx

--- src/componends/special.jsx
+++ src/componends/special.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./special.css";
-import video from "../assets/images/video.mp4";
-
 const BackgroundVideo = () => {
   return (
     <div className="video-container">
       <video className="background-video" autoPlay loop muted playsInline>
-        <source src="/assets/images/video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="video-overlay">
